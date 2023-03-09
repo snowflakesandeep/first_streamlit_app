72ff74efb85bd24b0324f9bb28238f348bb14cc3
snowflakesandeep <127436974+snowflakesandeep@users.noreply.github.com>
2023-03-09 13:32:43 -0500

    Update streamlit_app.py
    made an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamli.text('Hard-Boiled Free Range Egg')
